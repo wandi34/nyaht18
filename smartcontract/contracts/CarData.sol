07b11f1415f1cafbfca36b768af0e1dd56aec190
Simon Schmoll <ssc@adorsys.de>
2018-11-10 00:58:02 +0100

    wip

--- smartcontract/contracts/CarData.sol
+++ smartcontract/contracts/CarData.sol
@@ -1,17 +1,28 @@
 pragma solidity ^0.4.23;
 
-contract Migrations {
+contract CarData {
     address owner;
 
-	constructor () public {
-		this.owner = msg.sender;
-	}
+    mapping (uint8 => uint8) data;
+    constructor () public {
+    }
   // mapping from geolocation to hash
-	mapping (uint8=>uint8) hashlocation;
+//    mapping (uint8=>uint8) hashlocation;
 
-	function getHash(uint8 geolocation) public returns (uint8) {
-        require(0);
-		return 0;
-	}
+    // function getHash(uint8 geolocation) public returns (uint8) {
+    //   var location = hashlocation[geolocation];
+		//   return location;
+    // }
 
+    // function setHash(uint8 geolocation, uint8 ) public {
+    //   hashlocation[geolocation] = 
+    // }
+    function setWeather(uint8 _geolocation,uint8 _weather) public {
+        owner = msg.sender;
+        data[_geolocation] = _weather;
+    }
+
+    function getWeatherForGeolocation(uint8 _geolocation) view returns (uint8) {
+        return data[_geolocation];
+    }
 }
