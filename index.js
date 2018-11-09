2b05056b550860a32868e4cc75e63b4fdb7442e1
Andreas Winter <awi@adorsys.de>
2018-11-09 21:42:43 +0100

    wip

--- index.js
+++ index.js
@@ -2,22 +2,34 @@
 const http = require('http')
 const port = 8080
 
-const SerialPort = require("serialport").SerialPort
-const serialPort = new SerialPort("/dev/ttyACM0", {
-  baudrate: 57600
+const SerialPort = require("serialport")
+var serialport = new SerialPort('/dev/tty.usbmodem1421', {
+  baudRate: 9600
 });
 
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
+// const requestHandler = (request, response) => {
+//   console.log(request.url)
+//   response.end('Hello Node.js Server!')
+// }
 
-const server = http.createServer(requestHandler)
+// const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+// server.listen(port, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
 
-  console.log(`server is listening on ${port}`)
-})
+//   console.log(`server is listening on ${port}`)
+// })
+
+// it opens the connection and register an event 'data'
+serialport.on("open", function () {
+  console.log('Communication is on!');
+
+  // when your app receives data, this event is fired
+  // so you can capture the data and do what you need
+
+  serialport.on('data', function(data) {
+    console.log('data received: ' + data);
+  });
+});
