9a45712490c0add2fdd9c4db21303eb324a5f812
Andreas Winter <awi@adorsys.de>
2018-11-10 00:59:40 +0100

    Add web3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // content of index.js
 const http = require('http')
 const port = 8080
+const Web3 = require('web3');
+let web3;
 
 const SerialPort = require("serialport")
 var serialport = new SerialPort('/dev/tty.usbmodem1421', {
@@ -8,6 +10,18 @@ var serialport = new SerialPort('/dev/tty.usbmodem1421', {
 });
 
 const Readline = SerialPort.parsers.Readline;
+
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // Set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+web3.eth.defaultAccount = web3.eth.accounts[0];
+
+
+console.log("default account: ", web3.eth.defaultAccount);
 
 const parser = new Readline({delimiter: '\n'});
 serialport.pipe(parser);
