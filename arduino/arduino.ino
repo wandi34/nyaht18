5c8335511322f179edc8295fe0e753100e98d308
Simon Schmoll <ssc@adorsys.de>
2018-11-10 14:32:17 +0100

    wip

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,20 +1,26 @@
-void setup(){
-Serial.begin(9600);     // Communication started with 9600 baud
+void setup() {
+  Serial.begin(9600);     // Communication started with 9600 baud
 }
-void loop(){
-  
-delay(1000);
-int sensor=analogRead(A1); // Incoming analog signal read and appointed sensor
-Serial.println(sensor);   //Wrote serial port
-pinMode(LED_BUILTIN, OUTPUT);
-if(sensor > 700) {
+void loop() {
+  int inputThreshold = 500;
+  boolean rain = false;
+  //delay(10);
+  int alice = analogRead(A1); // Incoming analog signal read and appointed sensor
+  int bob = analogRead(A2);
+  int charlie = analogRead(A3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  if (alice > inputThreshold && bob > inputThreshold) {
     digitalWrite(LED_BUILTIN, HIGH);
-} else if (sensor >){
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
-  digitalWrite(LED_BUILTIN, HIGH);
-} else {
+    rain = true;
+  } else if (alice > inputThreshold && charlie > inputThreshold) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    rain = true;
+  } else if (bob > inputThreshold && charlie > inputThreshold) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    rain = true;
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+    rain = false;
+  }
   
-}
-
 }
